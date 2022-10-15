5f817a2dd34531bdddbf7e1c2d396859e422dc28
baver <bawer.talantimur@gmail.com>
2022-10-15 13:58:20 +0300

    Constructor Process

--- src/Oop/Class.java
+++ src/Oop/Class.java
@@ -11,6 +11,12 @@ public class Class {
         makine2.sayi1=10;
         System.out.println("makine2 " + makine2.sayi1);
 
+        Constructor c1 =new Constructor(5,6,"siyah");
+
+        c1.sayi1=10;
+        System.out.println(c1.sayi1+"---"+c1.sayi2);
+        System.out.println(c1.toplam());
+
 
     }
 
