7c9deff7c8360c5b3dd413d315d74928470a1a3e
baver <bawer.talantimur@gmail.com>
2022-10-21 21:13:48 +0300

    queue process

--- src/collections/PriorityQueueMain.java
+++ src/collections/PriorityQueueMain.java
@@ -1,2 +1,16 @@
-package collections;public class PriorityQueueMain {
+package collections;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueMain {
+    public static void main(String[] args) {
+        PriorityQueue q=new PriorityQueue();
+        q.add(80);
+        q.add(20);
+        q.add(10);
+        q.add(50);
+        while (!q.isEmpty()){
+            System.out.println(q.poll());
+        }
+    }
 }
